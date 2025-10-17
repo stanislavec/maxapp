c39368d5b9d116e7248a0192968e53f0b468ed0c
Крыжановский Станислав Константинович <KryzhanovskiiSK@alfanpf.ru>
2025-10-17 13:12:17 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/maxapp/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "add-query-to-assets",
+      transformIndexHtml(html) {
+        const timestamp = Date.now();
+        return html.replace(
+          /(src|href)="([^"]+\.(js|css))"/g,
+          `$1="$2?v=${timestamp}"`
+        );
+      },
+    },
+  ],
 });
