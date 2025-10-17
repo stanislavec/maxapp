2b7fe8091c09b4c7736dc456a745cf422db3fb3e
Крыжановский Станислав Константинович <KryzhanovskiiSK@alfanpf.ru>
2025-10-17 13:17:57 +0300

    config

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,37 @@
 import { Panel, CellSimple, Avatar, Typography, Flex } from "@maxhub/max-ui";
+import { useEffect } from "react";
 
-export const Home = ({ id, user }) => (
-  <Panel id={id}>
-    <Flex direction="column" align="center" gap={16}>
-      <Avatar.Container size={96} rightBottomCorner={<Avatar.OnlineDot />}>
-        <Avatar.Image fallback="ME" src={user.photo_url} />
-      </Avatar.Container>
-      <Flex className={styles.details} direction="column" align="center">
-        <Typography.Headline variant="large-strong">
-          {user.first_name}
-        </Typography.Headline>
+export const Home = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    console.log(window.WebApp);
+    if (window.WebApp.WebAppData) {
+      setData(window.WebApp.WebAppData);
+    }
+  }, []);
+
+  if (Object.keys(data).length == 0) return <>Нет данных</>;
+
+  console.log(data);
+
+  return (
+    <Panel id={id}>
+      <Flex direction="column" align="center" gap={16}>
+        <Avatar.Container size={96} rightBottomCorner={<Avatar.OnlineDot />}>
+          <Avatar.Image fallback="ME" src={user.photo_url} />
+        </Avatar.Container>
+        <Flex className={styles.details} direction="column" align="center">
+          <Typography.Headline variant="large-strong">
+            {user.first_name}
+          </Typography.Headline>
+        </Flex>
       </Flex>
-    </Flex>
-    <Flex>
-      <CellList mode="island">
-        <CellSimple onClick={onPDSclick} title="Заключить договор ПДС" />
-        <CellSimple onClick={onOPSClick} title="Оставить заявку на ОПС" />
-      </CellList>
-    </Flex>
-  </Panel>
-);
+      <Flex>
+        <CellList mode="island">
+          <CellSimple onClick={onPDSclick} title="Заключить договор ПДС" />
+          <CellSimple onClick={onOPSClick} title="Оставить заявку на ОПС" />
+        </CellList>
+      </Flex>
+    </Panel>
+  );
+};
