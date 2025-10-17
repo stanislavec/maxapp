e6cf2346e2e1a645afad559d6cd056d0846ee1a4
Крыжановский Станислав Константинович <KryzhanovskiiSK@alfanpf.ru>
2025-10-17 13:13:36 +0300

    config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { MaxUI } from "@maxhub/max-ui";
 import "@maxhub/max-ui/dist/styles.css";
+import { Home } from "./Home";
 
 const Root = () => (
   <MaxUI>
